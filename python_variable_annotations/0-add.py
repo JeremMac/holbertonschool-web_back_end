6fb925486a03e7e686d4215859bc5fe7c48e48fd
JeremMac <macal.jerem@gmail.com>
2024-01-03 10:11:21 +0100

    Added 0-add.py file.

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''
+A module that contains the typed version of add function.
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    function add that takes a float a and a float b as
+    arguments and returns their sum as a float
+    '''
+    return a + b
