8111281c36b4f9650a8adc3c394db353f57a9c3f
JeremMac <macal.jerem@gmail.com>
2024-01-03 15:30:07 +0100

    Added float keyword.

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,10 +6,11 @@ A module that define the asynchrone function wait_random.
 
 
 import asyncio
+from typing import Union
 from random import uniform
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: Union[int, float] = 10):
     '''
     an asynchronous coroutine that takes in an integer argument.
     '''
