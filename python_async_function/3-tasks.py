1a6ab1f440e35674eea8aa836747b57411bdd484
JeremMac <macal.jerem@gmail.com>
2024-01-04 12:11:59 +0100

    Added 3-tasks.py file.

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+'''
+A module that define the task_wait_random function.
+'''
+
+import asyncio
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''
+    A function task_wait_random that
+    takes an integer max_delay.
+
+    returns a asyncio.Task
+    '''
+    return asyncio.create_task(wait_random(max_delay))
