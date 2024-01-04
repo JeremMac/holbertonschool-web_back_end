58ab2f8f777b8ac565ff9c4835748e47722991d1
JeremMac <macal.jerem@gmail.com>
2024-01-04 14:21:32 +0100

    Changed while in for.

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,18 +8,16 @@ A module that defines the async_generator async function.
 
 import asyncio
 import random
-from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator():
     '''
     A function that loops ten times asynchronously
     waits for one second then yield  random
     number between 0 and 10.
     '''
     i = 0
-    while i < 10:
+    for _ in range(10):
         await asyncio.sleep(1)
-        rand_num = random.uniform(0, 10)
-        yield rand_num
+        yield random.uniform(0, 10)
         i += 1
