3e9668759d18fe338592298751559be295694c4d
JeremMac <macal.jerem@gmail.com>
2024-01-05 12:19:39 +0100

    Fix: documentation and pycodestyle.

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,15 +2,24 @@
 
 
 '''
-A module that defines 
+A module that defines measure_runtime coroutine.
 '''
 
 
-import asyncio, time, typing
+import asyncio
+import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    '''
+    A measure_runtime coroutine that will
+    execute async_comprehension four times
+    in parallel using asyncio.gather.
+
+    Returns the total time execution of async_comprehension
+    async function.
+    '''
     start_time = time.time()
     gather_routine = [async_comprehension() for _ in range(4)]
     await asyncio.gather(*gather_routine)
