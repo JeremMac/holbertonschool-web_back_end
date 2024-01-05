6ecf5585d89de4d04bebc27846080ba7c1975e40
JeremMac <macal.jerem@gmail.com>
2024-01-05 12:14:32 +0100

    Deleted loop for async code.

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -6,14 +6,14 @@ A module that defines
 '''
 
 
-import asyncio, time
+import asyncio, time, typing
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    for _ in range(4):
-        start_time = time.time()
-        await asyncio.gather(async_comprehension())
-        end_time = time.time()
+    start_time = time.time()
+    gather_routine = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*gather_routine)
+    end_time = time.time()
     total_time = end_time - start_time
     return total_time
