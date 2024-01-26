b32fd64b79a83a3b81fdf43fb54a0d602be2db82
JeremMac <macal.jerem@gmail.com>
2024-01-26 10:15:35 +0100

    Fix: error message.

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,14 @@
-#!/usr/bin/node
-
-/* eslint-disable */
 export default function handleResponseFromAPI(promise) {
   promise
-    .then((response) => {
+    .then(() => {
       console.log('Got a response from the API');
       return {
         status: 200,
         body: 'success',
       };
     })
-    .catch((error) => new Error());
+    .catch((error) => {
+      console.error(`API request failed:`, error);
+      return new Error();
+    });
 }
