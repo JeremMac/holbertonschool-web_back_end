626e3a8cbc6fc180e4ea5d999c2b62eb810a94aa
JeremMac <macal.jerem@gmail.com>
2024-01-29 12:56:33 +0100

    Fix: eslint coding style.

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 export default function getStudentsByLocation(studentList, city) {
-    const MyArray = studentList.filter((key) => key.location === city);
-    return MyArray;
+  const MyArray = studentList.filter((key) => key.location === city);
+  return MyArray;
 }
