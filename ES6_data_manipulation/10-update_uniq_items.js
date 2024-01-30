a459b70672a145fdb0e457973ae418a39f28bf81
JeremMac <macal.jerem@gmail.com>
2024-01-30 13:02:46 +0100

    Added error condition.

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(MyMap) {
+  if (!MyMap) {
+    throw new Error('Cannot process');
+  }
   for (const [key, value] of MyMap) {
     if (value === 1) {
       MyMap.set(key, 100);
